a9ad455c664722f28dc8aadd870488d5f5c09e9d
Yhemen5525 <51065060+Yhemen5525@users.noreply.github.com>
2020-11-28 21:05:52 -1200

    Form component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import "./styles.css";
+import "./App.css";
+//importing components
+import Form from "./components/Form.js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <header>Igot Todo-App</header>
+      <Form />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
