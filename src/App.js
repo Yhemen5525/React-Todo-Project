7bc782c16e6b0c9f403c62b6072fd5c732a81c1b
Yhemen5525 <51065060+Yhemen5525@users.noreply.github.com>
2020-11-28 22:32:19 -1200

    useState used

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 //importing components
-import Form from "./components/Form.js";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 export default function App() {
+  const [inputText, setInputText] = useState("");
+
   return (
     <div className="App">
       <header>Igot Todo-App</header>
-      <Form />
+      <Form setInputText={setInputText} />
+      <TodoList />
+      <h2>{inputText}</h2>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const Form = function () {
+const Form = function ({ setInputText }) {
+  const textInputHandler = function (e) {
+    setInputText(e.target.value);
+  };
+
   return (
     <form>
-      <input type="text" className="todo-input" />
+      <input onChange={textInputHandler} type="text" className="todo-input" />
       <button className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
